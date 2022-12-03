e2db1dadeb322f0663833e3482f069ec354ad2c4
zengxiling <1905359611@qq.com>
2022-12-03 21:03:51 +0800

    for循环

---
+++ fortest.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main(){
+	/*for (;;){
+		printf("hehe");//死循环
+	}*/
+	/*
+	int i = 0;
+	int j = 0;
+	for (; i < 10; i++)
+	{
+		for (; j < 10; j++)
+		{
+			printf("hehe");
+		}
+	}//打印10个hehe*/
+	/*int x, y;
+	for (x = 0, y = 0; x<2 && y<5; ++x, ++y){
+		printf("hehe\n");
+
+		}//打印两个hehe*/
+	int i = 0;
+	int k = 0;
+	for (i = 0, k = 0; k = 0; i++, k++)
+		k++;
+	return 0;
+}
