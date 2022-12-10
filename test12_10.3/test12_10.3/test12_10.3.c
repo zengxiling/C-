90cbe4497abdf08ed0116ca304adeb0eb73cb491
zengxiling <1905359611@qq.com>
2022-12-10 20:43:27 +0800

    打印三个数中从大的小

---
+++ test12_10.3/test12_10.3/test12_10.3.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int a;
+	int b;
+	int c;
+	printf("请输入三个数:");
+	scanf("%d %d %d", &a, &b, &c);
+	if (a < b)
+	{
+		int temp = a;
+		a = b;
+		b = temp;
+	}
+	if (a < c)
+	{
+		int temp = a;
+		a = c;
+		c = temp;
+	}
+	if (b < c)
+	{
+		int temp = b;
+		b = c;
+		c = temp;
+	}
+	printf("%d%d%d", a, b, c);
+	return 0;
+}
